aead0abda0be096f47bb35288204972761d86591
XAngeloidX <58824199+XAngeloidX@users.noreply.github.com>
2023-06-13 01:48:46 -0700

    Sending out the JS file that will give an alert once opening the homepage of the website.

--- personal_site/javascript.js
+++ personal_site/javascript.js
@@ -1,3 +1,4 @@
-/*
-    step1:
-*/
+//alert that will play upon opening the website homepage
+alert(
+  "Hiya, if your reading this welcome to the demo of a website that will be done in the future thanks for checkin my progress."
+);
